5ef5e77c8dfbcde11259ab984a9f93e33ab4aeb1
hbrazier01 <59978579+hbrazier01@users.noreply.github.com>
2026-05-14 18:17:35 -0600

    Delete packages directory

--- packages/api/airlog/src/common/types.ts
+++
@@ -1,3 +0,0 @@
-// Keep types loose for v1 bootstrap. We'll tighten once the API works end-to-end.
-export type AirlogProviders = any;
-export type DeployedAirlogContract = any;
--- packages/cli/airlog/src/undeployed-local.ts
+++
@@ -1,6 +0,0 @@
-import { StandaloneConfig, createLogger } from '@repo/kitties-api';
-import { run } from './run.js';
-
-const config = new StandaloneConfig();
-const logger = await createLogger(config.logDir);
-await run(config, logger);
